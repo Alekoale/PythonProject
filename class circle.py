543e0f971ee01a9a5bbee2e25fcd069d5d03cb59
alexomondi18248@gmail.com <alexomondi18248@gmail.com>
2025-09-09 15:59:29 +0300

    circle

---
+++ class circle.py
@@ -0,0 +1,9 @@
+import sys
+class Mycircle:
+    def __init__(self, radius, position=(0, 0)):
+        self.diameter = 2 * radius
+        self.position = position
+
+
+def perimeter(Mycircle):
+  perimeter = 2 * (22/7) *
